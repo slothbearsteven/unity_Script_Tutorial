876cb044dbde6cb3efc0bfd5347d7bcd5dc54052
slothbearsteven <umbrylax@gmail.com>
2022-01-29 00:43:30 -0700

    Added  Transformations

--- Assets/blockMove.cs
+++ Assets/blockMove.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallTransform : MonoBehaviour
+public class blockMove : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector3 scaleChange;
+    public Vector3 positionChange;
+
     void Start()
     {
 
@@ -14,6 +15,6 @@ public class BallTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localScale += scaleChange;
+        transform.position += positionChange;
     }
 }
